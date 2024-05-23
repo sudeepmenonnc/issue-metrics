d4e6df8fefbf9a82bd67304e99451470dd6ff2b0
sudeepmenonnc <sudeepm@gmail.com>
2024-05-23 10:24:51 -0400

    Update test_issue_metrics.py

--- test_issue_metrics.py
+++ test_issue_metrics.py
@@ -30,6 +30,7 @@ from issue_metrics import (
 
 class TestSearchIssues(unittest.TestCase):
     """Unit tests for the search_issues function.
+    asdfasdfasdfasdfasdfasdfasdf
 
     This class contains unit tests for the search_issues function in the
     issue_metrics module. The tests use the unittest module and the unittest.mock
